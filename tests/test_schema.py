355fed853df7cb1558bfc97688fe4a925aa7f529
Emily Lockhart <validitymaiden@gmail.com>
2022-03-02 14:40:22 -0500

    add more tests for hellofresh

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,3 +1,6 @@
+import pytest
+import schema
+
 import recipy.schemas
 
 
@@ -7,3 +10,20 @@ class TestHelloFresh():
         hf = recipy.schemas.HelloFreshSchema()
         for attr in recipy.schemas.HelloFreshSchema.specification:
             assert attr in hf.schema
+
+    def test_good_data(self):
+        hf = recipy.schemas.HelloFreshSchema()
+        data = {"name": "emily"}
+        assert hf.validate(data)
+
+    def test_bad_data_value(self):
+        hf = recipy.schemas.HelloFreshSchema()
+        data = {"name": 1}
+        with pytest.raises(schema.SchemaError):
+            hf.validate(data)
+
+    def test_bad_data_key(self):
+        hf = recipy.schemas.HelloFreshSchema()
+        data = {"schemaTest": True}
+        with pytest.raises(schema.SchemaMissingKeyError):
+            hf.validate(data)
