684f5396beb768b74d09c96586477cf4faf69839
Emily Lockhart <validitymaiden@gmail.com>
2022-03-05 10:04:07 -0500

    change schema tests to handle changes in schema while still testing functionality
    add function to create data based upon a given schema
    
    change tests to alter test data rather than provide full test set
    
    parameterize attribute tests

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -4,6 +4,19 @@ import schema
 import recipy.schemas
 
 
+def make_schema_data(_schema: schema.Schema) -> dict:
+    """create skeleton data according to given schema"""
+    data = {}
+    for (k, v) in _schema.schema.items():
+        if issubclass(v, str):
+            data[k] = ''
+        elif issubclass(v, list):
+            data[k] = []
+        else:
+            raise Exception("Can't create data for unknown type: {0}".format(v))
+    return data
+
+
 class TestPaprika:
     """tests for PaprikaSchema"""
     def test_spec(self):
@@ -11,23 +24,25 @@ class TestPaprika:
         for attr in recipy.schemas.PaprikaSchema.specification:
             assert attr in s.schema
 
-    def test_attrs(self):
+    def test_attrs_method(self):
         s = recipy.schemas.PaprikaSchema()
         assert s.attrs == s.schema
 
-    def test_good_data(self):
-        s = recipy.schemas.PaprikaSchema()
-        data = {"name": "emily", "prep_time": "forever"}
-        assert s.validate(data)
+    @pytest.mark.parametrize("attr, test", [("name", "Emily"), ("prep_time", "x minutes")])
+    def test_attr(self, attr, test):
+        spec = recipy.schemas.PaprikaSchema.specification.get(attr)
+        assert schema.Schema(spec).validate(test)
 
-    def test_bad_data_value(self):
+    def test_wrong_data_type(self):
         s = recipy.schemas.PaprikaSchema()
-        data = {"name": 1, "prep_time": "forever"}
+        data = make_schema_data(s)
+        data['name'] = 1
         with pytest.raises(schema.SchemaError):
             s.validate(data)
 
-    def test_bad_data_key(self):
+    def test_wrong_key_present(self):
         s = recipy.schemas.PaprikaSchema()
-        data = {"schemaTest": True, "name": "emily", "prep_time": "forever"}
+        data = make_schema_data(s)
+        data['notSupposedToBeHere'] = True
         with pytest.raises(schema.SchemaWrongKeyError):
             s.validate(data)
