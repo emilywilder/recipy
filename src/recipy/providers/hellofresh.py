ce492fdf84402868fef679769431566e3b6e7196
Emily Lockhart <validitymaiden@gmail.com>
2022-03-06 09:40:20 -0500

    favor find() over select() for bs4 objects

--- src/recipy/providers/hellofresh.py
+++ src/recipy/providers/hellofresh.py
@@ -17,7 +17,7 @@ class HelloFresh(base.BaseProvider):
 
     @property
     def servings(self) -> str:
-        tag = self.soup.select_one("[data-test-id='serving-amount-container']")
+        tag = self.soup.find('div', attrs={'data-test-id': 'serving-amount-container'})
         divs = tag.find_all("div")
         return "{0} to {1} servings".format(
             divs[2].get_text().strip(),
@@ -34,7 +34,7 @@ class HelloFresh(base.BaseProvider):
 
     @property
     def prep_time(self) -> str:
-        tag = self.soup.select_one("[data-translation-id='recipe-detail.cooking-time']")
+        tag = self.soup.find('span', attrs={'data-translation-id': 'recipe-detail.cooking-time'})
         return tag.find_parent().find_next_sibling().get_text().strip()
 
     @property
