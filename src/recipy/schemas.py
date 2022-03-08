07bb4da83e590cd9925fdb052840913b1d05ad4e
Emily Lockhart <validitymaiden@gmail.com>
2022-03-08 14:15:58 -0500

    add full schema description (was missing last entries due to not scrolling lol)

--- src/recipy/schemas.py
+++ src/recipy/schemas.py
@@ -20,24 +20,32 @@ class PaprikaSchema(BaseSchema):
 
     As per their specification (https://www.paprikaapp.com/help/mac/#importrecipes):
 
-name: My Tasty Recipe
-servings: 4-6 servings
-source: Food Network
-source_url: http://www.google.com
-prep_time: 10 min
-cook_time: 30 min
-on_favorites: yes
-categories: [Dinner, Holiday]
-nutritional_info: 500 calories
-difficulty: Easy
-rating: 5
-notes: |
-  This is delicious!!!
-photo: (base-64 encoded image)
-ingredients: |
-  1/2 lb meat
-  1/2 lb vegetables
-  salt
+    name: My Tasty Recipe
+    servings: 4-6 servings
+    source: Food Network
+    source_url: http://www.google.com
+    prep_time: 10 min
+    cook_time: 30 min
+    on_favorites: yes
+    categories: [Dinner, Holiday]
+    nutritional_info: 500 calories
+    difficulty: Easy
+    rating: 5
+    notes: |
+      This is delicious!!!
+    photo: (base-64 encoded image)
+    ingredients: |
+      1/2 lb meat
+      1/2 lb vegetables
+      salt
+      pepper
+      2 tbsp olive oil
+      4 cups flour
+    directions: |
+      Mix things together.
+      Eat.
+      Tasty.
+      Yum yum yum.
     """
     specification = {
         "name": str,
@@ -53,6 +61,6 @@ ingredients: |
         # "rating": int,
         "notes": yaml.Literal,
         "photo": str,
-        "ingredients": yaml.Literal
+        "ingredients": yaml.Literal,
     }
 
